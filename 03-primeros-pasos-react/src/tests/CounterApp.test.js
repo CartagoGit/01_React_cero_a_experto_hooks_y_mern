a925c3ef5d47db93d07adb7956409ec2bb26056a
Cartago <cartago.relaxingcup@gmail.com>
2021-11-25 13:16:36 +0100

    S05-C64- Hacemos pruebas/tests de los botones de CounterApp, añadimos el recurso con el codigo, y terminamos la seccion

--- 03-primeros-pasos-react/src/tests/CounterApp.test.js
+++ 03-primeros-pasos-react/src/tests/CounterApp.test.js
@@ -5,19 +5,54 @@ import { shallow } from "enzyme";
 import CounterApp from "../CounterApp";
 
 describe("Pruebas con CounterAPP", () => {
+    let wrap;
+    beforeEach(()=>{
+        wrap = shallow(<CounterApp />);
+        // console.log(wrap.text());
+    });
 	test("Deberia mostrar counterapp correctamente con un snapshoot", () => {
 		// const valor = 123;
-		const wrapper = shallow(<CounterApp />);
+		// const wrapper = shallow(<CounterApp />);
 
-		expect(wrapper).toMatchSnapshot();
+		expect(wrap).toMatchSnapshot();
 	});
 
 	test("Deberia mostrar el valor por defecto 100", () => {
 		const valor = 100;
-		const wrapper = shallow(<CounterApp value={valor} />);
-		wrapper.find("h2").forEach((node) => {
-            console.log(node.text());
+		const wrap = shallow(<CounterApp value={valor} />);
+		wrap.find("h2").forEach((node) => {
+            // console.log(node.text());
 			expect(parseInt(node.text().trim())).toBe(valor);
 		});
 	});
+
+    
+    test('Debe incrementar el contador con el boton +1', () => {
+        wrap.find('button').at(0).simulate('click');
+        expect(parseInt(wrap.find("h2").at(1).text().trim())).toBe(1);
+
+    });
+
+    test('Debe decrementar el contador con el boton -1', () => {
+        wrap.find('button').at(2).simulate('click');
+        expect(parseInt(wrap.find("h2").at(1).text().trim())).toBe(-1);
+
+    });
+
+    test('Debe comprobar que el boton reset coloca el valor por defecto', () => {
+        const valor = 100;
+        const wrap = shallow(<CounterApp value={valor} />);
+        wrap.find('button').at(0).simulate('click');
+        
+        wrap.find('button').at(0).simulate('click');
+        wrap.find('button').at(0).simulate('click');
+        wrap.find('button').at(1).simulate('click');
+
+        const counterNum = parseInt(wrap.find("h2").at(1).text().trim());
+
+        expect (counterNum).toBe(valor)
+
+    })
+    
+    
 });
