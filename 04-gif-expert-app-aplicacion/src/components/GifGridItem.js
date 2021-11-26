89e39b96c03278ad420b8f273957bbb06311f1aa
Cartago <cartago.relaxingcup@gmail.com>
2021-11-26 09:23:14 +0100

    S08-C88- Creamos los archivos necesarios para empezar las pruebas de tests de GiffExpertApp

--- 04-gif-expert-app-aplicacion/src/components/GifGridItem.js
+++ 04-gif-expert-app-aplicacion/src/components/GifGridItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const GifGridItem = ({id, title, url}) => {
     // console.log(id,title,url);
     return (
-        <div className='card animate__tada'>
+        <div className='card animate__tada' id={id}>
             {/* {props} */}
             <img src={url} alt={title}/>
             <p>{title}</p>
